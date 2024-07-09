167ce5b3f7dabb156a2d8300ba4bbbfa8e422031
angelR003 <220432@utxicotepec.edu.mx>
2024-07-09 13:54:13 -0600

    Correciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Login from './views/login'; // Asegúrate de que la ruta es correcta
-import Inicio from './views/Inicio'; // Asegúrate de que la ruta es correcta
+import Home from './views/Home'; // Asegúrate de que la ruta es correcta
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/src/views/home.jsx" element={<Inicio />} />
+      <Route path="/Home" element={<Home />} />
     </Routes>
   );
 }
