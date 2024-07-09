b5fbd9d7c8ce3e4246df8068804acbc0674e358e
Ederdal <degarcruz2004@gmail.com>
2024-07-09 09:35:19 -0600

    upd: creando una mejor estructura para poder tener una mejor organización así mismo creando nuevos archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Login  from '../src/useFetch.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Login from './views/login';
+import Home from './views/home';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/home">Home</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/views/login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import useLogin from '../controllers/useLogin';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState('');
+  const handleLogin = useLogin(email, password, setLoginError);
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="correo">Usuario o email</label>
+        <input 
+          type="email"
+          id="correo"
+          placeholder="Ingrese su correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="contrasena">Contraseña</label>
+        <input 
+          type="password"
+          id="contrasena"
+          placeholder="Ingrese su contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Iniciar sesión</button>
+        {loginError && <p>{loginError}</p>}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
