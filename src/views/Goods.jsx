14e659fd0e908a3529e3132cead39616e338a865
Ederdal <degarcruz2004@gmail.com>
2024-07-10 14:53:20 -0600

    upd: mostrando las imagenes en la vista de bienes

--- src/views/Goods.jsx
+++ src/views/Goods.jsx
@@ -1,57 +1,58 @@
 import { useEffect, useState } from "react";
 
-function Goods(){
+function Goods() {
     const [datos, setDatos] = useState([]);
 
     useEffect(() => {
         fetch("/api/bienes/bienesadquiridos")
-       .then(response => response.json())
-       .then(data => setDatos(data))
-       .catch (error => console.error("Error al cargar los datos",error));
+        .then(response => response.json())
+        .then(data => setDatos(data))
+        .catch(error => console.error("Error al cargar los datos", error));
     }, []);
 
-return(
-    <div>
-        <h1>Bienes Adquiridos</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Modelo</th>
-                    <th>Caracteristicas</th>
-                    <th>Marca</th>
-                    <th>Numero de serie</th>
-                    <th>Numero de inventario</th>
-                    <th>Origen</th>
-                    <th>Foto</th>
-                    <th>Ubicacion</th>
-                    <th>Depreciacion</th>
-                    <th>Valor</th>
-                </tr>
-            </thead>
-            <tbody>
-                {datos.map((dato) => (
-                    <tr key={dato.id}>
-                        <td>{dato.id}</td>
-                        <td>{dato.name}</td>
-                        <td>{dato.model}</td>
-                        <td>{dato.characteristics}</td>
-                        <td>{dato.brand}</td>
-                        <td>{dato.serial_number}</td>
-                        <td>{dato.inventory_number}</td>
-                        <td>{dato.origin}</td>
-                        <td>{dato.photo}</td>
-                        <td>{dato.ubication}</td>
-                        <td>{dato.accumulated_depreciation}</td>
-                        <td>{dato.value}</td>
+    return (
+        <div>
+            <h1>Bienes Adquiridos</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Modelo</th>
+                        <th>Características</th>
+                        <th>Marca</th>
+                        <th>Número de serie</th>
+                        <th>Número de inventario</th>
+                        <th>Origen</th>
+                        <th>Foto</th>
+                        <th>Ubicación</th>
+                        <th>Depreciación</th>
+                        <th>Valor</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-)
-
+                </thead>
+                <tbody>
+                    {datos.map((dato) => (
+                        <tr key={dato.id}>
+                            <td>{dato.id}</td>
+                            <td>{dato.name}</td>
+                            <td>{dato.model}</td>
+                            <td>{dato.characteristics}</td>
+                            <td>{dato.brand}</td>
+                            <td>{dato.serial_number}</td>
+                            <td>{dato.inventory_number}</td>
+                            <td>{dato.origin}</td>
+                            <td>
+                                <img src={dato.photo} alt={`Foto de ${dato.name}`} width="100" />
+                            </td>
+                            <td>{dato.ubication}</td>
+                            <td>{dato.accumulated_depreciation}</td>
+                            <td>{dato.value}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Goods;
