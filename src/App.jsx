4eca00ebd3c5a7ab89d6378458de0b989ebaf6f6
Ederdal <degarcruz2004@gmail.com>
2024-07-10 10:39:22 -0600

    add: crenado las vistas del Header,  Home, y  Login, así mismo el header lo mando a llamar solo en las vitas que requiere

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from './views/login'; // Asegúrate de que la ruta es correcta
-import Home from './views/Home'; // Asegúrate de que la ruta es correcta
-import { Routes, Route } from 'react-router-dom';
+import Home from './views/home'; // Asegúrate de que la ruta es correcta
+import User from './views/formUser';
+import Header from './components/Header'; // Importa el componente Header aquí
 
 function App() {
+  const location = useLocation(); // Obtiene la ubicación actual de la ruta
+
+  // Definir las rutas donde se debe mostrar el Header
+  const showHeaderRoutes = ['/Home', '/User'];
+
+  // Verificar si la ruta actual está en showHeaderRoutes
+  const showHeader = showHeaderRoutes.includes(location.pathname);
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Home" element={<Home />} />
-    </Routes>
+    <div>
+      {showHeader && <Header />} 
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/User" element={<User />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
