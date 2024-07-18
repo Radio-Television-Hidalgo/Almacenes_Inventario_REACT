e39a6485b3ee05fb2a271875dc5f95cd6e416984
Droid636 <2022043@utsh.edu.mx>
2024-07-18 10:48:57 -0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Header.css'; // Importa los estilos CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS
+import '../styles/Header.css'; // Importa los estilos CSS personalizados
 
 const Header = () => {
   return (
- <div>
-    <header className="navbar">
-      <div className="logo-container">
-        <div className="logo">
-          <h1>Bienvenido</h1>
-          <p>Sistema de inventarios</p>
+    <div>
+      <nav>
+        <div className="header-top">
+          <h1 className="invent">Bienvenido</h1>
+          <div className="user-info">
+            <img
+              className="user-avatar"
+              src="https://media.gettyimages.com/id/75600163/photo/the-36th-annual-naacp-image-awards-portraits.jpg?b=1&s=594x594&w=0&k=20&c=SbkJPZ3_UF9GVzr8bn2nZ0q0mbNtJNJxqYa7NygCFwQ="
+              alt="Avatar del usuario"
+            />
+            <p>Nombre de usuario</p>
+          </div>
         </div>
-        <div className="user-info">
-          <img 
-            className="user-avatar" 
-            src="https://media.gettyimages.com/id/75600163/photo/the-36th-annual-naacp-image-awards-portraits.jpg?b=1&s=594x594&w=0&k=20&c=SbkJPZ3_UF9GVzr8bn2nZ0q0mbNtJNJxqYa7NygCFwQ=" 
-            alt="Avatar del usuario" 
-          />
-          <p>Nombre de usuario</p>
+        <div className="header-top">
+          <p className="header-paragraph">Sistema inventario y Almacen de Radio y Televisión de Hidalgo</p>
         </div>
-      </div>
-      
-      
-    </header>
-    <nav className="nav-links">
-        <ul>
-          <li><Link to="/">Salir</Link></li>
-          <li><Link to="/Home">Home</Link></li>
-          <li><Link to="/User">User</Link></li>
-            <li><Link to="/Bills">Facturas</Link></li>
-            <li><Link to="/StateofThegoods">Estado de los productos</Link></li>
-            <li><Link to="/Assignations">Asignaciones</Link></li>
-          <li><Link to="/Goods">Goods</Link></li>
-          <li><Link to="/SeeUser">ver Usuarios</Link></li>
-        </ul>
+        
+         
+        
+        <div className="header-bottom">
+          <input type="checkbox" id="sidebar-active" />
+          <label htmlFor="sidebar-active" className="open-sidebar-button">
+            <span>☰</span>
+          </label>
+          <label id="overlay" htmlFor="sidebar-active"></label>
+          <div className="links-container">
+            <Link to="/Home">Home</Link>
+            <Link to="/User">User</Link>
+            <Link to="/Bills">Facturas</Link>
+            <Link to="/StateofThegoods">Estado de los productos</Link>
+            <Link to="/Assignations">Asignaciones</Link>
+            <Link to="/Goods">Goods</Link>
+            <Link to="/SeeUser">Ver Usuarios</Link>
+            <Link to="/">Salir</Link>
+          </div>
+        </div>
       </nav>
     </div>
   );
