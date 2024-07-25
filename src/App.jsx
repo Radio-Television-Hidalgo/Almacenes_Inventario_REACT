b3a884670f4f05114beed967b1180a746f712a81
ZaS-ex <a.zacek.gc@gmail.com>
2024-07-25 11:26:31 -0600

    vista de entregas pendientes

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import Articles from "./views/articles/Articles";
 import PolicyScreen from "./views/articles/policyScreen";
 import Footer from "./components/Footer";
 import GeneralReceipt from "./views/inventory/generalReceipt";
+import PendingDeliveries from "./views/deliveries/pendingDeliveries";
 
 const showHeaderRoutes = [
   "/inicio",
@@ -51,7 +52,8 @@ const showHeaderRoutes = [
   "/articulos",
   "/assignations",
   "/polizas",
-  "/resguardoGeneral"
+  "/resguardoGeneral",
+  "/entregasPendientes"
 ];
 
 const routeTitles = {
@@ -78,6 +80,7 @@ const routeTitles = {
   "/inventarios/usuario": "Inventarios de Usuarios",
   "/articulos": "Lista de Bienes",
   "/resguardoGeneral": "Resguardo General",
+  "/entregasPendientes": "Entregas Pendientes"
 };
 
 const App = () => {
@@ -116,6 +119,7 @@ const App = () => {
             <Route path="/inventarios/usuario" element={<UserInventory />} />
             <Route path="/Bajadebien" element={<DeregistrationofMaterial />} />
             <Route path="/resguardoGeneral" element={<GeneralReceipt />} />
+            <Route path="/entregasPendientes" element={<PendingDeliveries />} />
             <Route
               path="/articulos/:inventoryNumber"
               element={<ArticleDetails />}
---
+++ src/views/deliveries/pendingDeliveries.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import React from 'react';
+
+function PendingDeliveries() {
+    const [datos, setDatos] = useState([]);
+
+    const fetchDatos = () => {
+        fetch("/api/entregas/gestionarEntregas")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error al cargar los datos: " + response.status);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setDatos(data);
+            })
+            .catch((error) => {
+                console.error("Error al cargar los datos", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchDatos();
+    }, []);
+
+    return (
+        <div>pendingDeliveries
+            <h1>Entregas pendientes</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Articulo</th>
+                        <th>Cantidad</th>
+                        <th>Fecha de entrega</th>
+                        <th>Descripcion</th>
+                        <th>Fotos</th>
+                        <th>Usuario que entrego</th>
+                        <th>Usuario que recibio</th>
+                        <th>Estatus de entrega</th>
+                        <th>Finalizar</th>
+                        <th>Cancelar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {datos.map((dato) => (
+                        <tr key={dato.id}>
+                            <td>{dato.tb_articulo.name}</td>
+                            <td>{dato.quantity}</td>
+                            <td>{dato.delivery_date}</td>
+                            <td>{dato.description}</td>
+                            <td><img src={`${dato.photos_entrance}`} alt={`foto de ${dato.tb_articulo.name}`} /></td>
+                            <td>{dato.user_deliver.name}</td>
+                            <td>{dato.user_receive.name}</td>
+                            <td>{dato.status}</td>
+                            <td><button>Finalizar</button></td>
+                            <td><button>Cancelar</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PendingDeliveries;
