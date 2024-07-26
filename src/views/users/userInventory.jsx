9dec06f73c3683ffabaac5717a3654932349b280
MendezPro <orlanmen58@gmail.com>
2024-07-26 11:01:22 -0600

    Crds de inventario de usuario

--- src/views/users/userInventory.jsx
+++ src/views/users/userInventory.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import '../../styles/userInventory.css'; // Asegúrate de que la ruta sea correcta
 
 function UserInventory() {
     const [datos, setDatos] = useState([]);
@@ -33,7 +34,7 @@ function UserInventory() {
     const groupedData = datos.reduce((acc, dato) => {
         if (!acc[dato.user_id]) {
             acc[dato.user_id] = {
-                user: dato.user, // Asegúrate de que el dato tiene la estructura correcta
+                user: dato.user,
                 items: []
             };
         }
@@ -47,7 +48,7 @@ function UserInventory() {
 
     const closeModal = () => {
         setModalData(null);
-        setSearchTerm(""); // Clear search term when closing the modal
+        setSearchTerm("");
     };
 
     const filteredModalData = modalData
@@ -62,77 +63,74 @@ function UserInventory() {
     return (
         <div style={{ padding: '20px', display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
             {Object.values(groupedData).map(({ user, items }) => (
-                <div key={user.worker_nomber} style={styles.card}>
-                    <div style={styles.userInfo}>
-                        <img src={user.img} alt="Foto del Usuario" style={styles.userImage} />
+                <div key={user.worker_nomber} className="card-u">
+                    <div className="userInfo-u">
+                        <img src={user.img} alt="Foto del Usuario" className="userImage" />
                         <div>
-                            <p style={styles.userText}><strong>Nombre:</strong> {user.name}</p>
-                            <p style={styles.userText}><strong>Número de Trabajador:</strong> {user.worker_nomber}</p>
-                            <p style={styles.userText}><strong>Tipo:</strong> {user.type}</p>
+                            <p className="C"><strong>Nombre:</strong> {user.name}</p>
+                            <p className="userText"><strong>Número de Trabajador:</strong> {user.worker_nomber}</p>
+                            <p className="userText"><strong>Tipo:</strong> {user.type}</p>
                         </div>
                     </div>
-                    <button style={styles.button} onClick={() => handleViewMore(items)}>
+                    <button className="button-u" onClick={() => handleViewMore(items)}>
                         Ver Más
                     </button>
                 </div>
             ))}
-            {/* Modal */}
             {modalData && (
-                <div style={styles.modal}>
-                    <div style={styles.modalContent}>
-                        <span style={styles.closeButton} onClick={closeModal}>&times;</span>
+                <div className="modal-u">
+                    <div className="modalContent-u">
+                        <span className="closeButton" onClick={closeModal}>&times;</span>
                         <input
                             type="text"
                             placeholder="Buscar por nombre"
-                            style={styles.searchInput}
+                            className="searchInput"
                             value={searchTerm}
                             onChange={(e) => setSearchTerm(e.target.value)}
                         />
-                        <table style={styles.table}>
+                        <table className="table-u">
                             <thead>
                                 <tr>
-                                    <th style={styles.tableHeaderCell}>IMG</th>
-                                    <th style={styles.tableHeaderCell}>Nombre</th>
-                                    <th style={styles.tableHeaderCell}>Descripción</th>
-                                    <th style={styles.tableHeaderCell}>Categoría</th>
-                                    <th style={styles.tableHeaderCell}>Fecha de Entrada</th>
-                                    <th style={styles.tableHeaderCell}>Fecha de Salida</th>
-                                    <th style={styles.tableHeaderCell}>Locación</th>
-                                    <th style={styles.tableHeaderCell}>Estatus</th>
-                                    <th style={styles.tableHeaderCell}>Fecha de Resguardo</th>
-                                    <th style={styles.tableHeaderCell}>Motivo no Asignado</th>
-                                    <th style={styles.tableHeaderCell}>Comentario</th>
-                                    <th style={styles.tableHeaderCell}>Número de Serie</th>
-                                    <th style={styles.tableHeaderCell}>Número de Inventario</th>
+                                    <th className="tableHeaderCell">IMG</th>
+                                    <th className="tableHeaderCell">Nombre</th>
+                                    <th className="tableHeaderCell">Descripción</th>
+                                    <th className="tableHeaderCell">Categoría</th>
+                                    <th className="tableHeaderCell">Fecha de Entrada</th>
+                                    <th className="tableHeaderCell">Fecha de Salida</th>
+                                    <th className="tableHeaderCell">Locación</th>
+                                    <th className="tableHeaderCell">Estatus</th>
+                                    <th className="tableHeaderCell">Fecha de Resguardo</th>
+                                    <th className="tableHeaderCell">Motivo no Asignado</th>
+                                    <th className="tableHeaderCell">Comentario</th>
+                                    <th className="tableHeaderCell">Número de Serie</th>
+                                    <th className="tableHeaderCell">Número de Inventario</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {filteredModalData.map(item => (
-                                    <tr key={item.id} style={styles.tableRow}>
-                                        <td style={styles.tableCell}>
+                                    <tr key={item.id} className="tableRow">
+                                        <td className="tableCell">
                                             <img
-                                                src={item.articles.photos_entry} // Utiliza la ruta completa desde el backend
+                                                src={item.articles.photos_entry}
                                                 alt="Foto del Artículo"
-                                                style={styles.userImage} // Aplica el estilo de imagen
+                                                className="userImage"
                                             />
                                         </td>
-                                        <td style={styles.tableCell}>{item.name}</td>
-                                        <td style={styles.tableCell}>{item.description}</td>
-                                        <td style={styles.tableCell}>{item.category}</td>
-                                        <td style={styles.tableCell}>{new Date(item.entry_date).toLocaleDateString()}</td>
-                                        <td style={styles.tableCell}>{new Date(item.exit_date).toLocaleDateString()}</td>
-                                        <td style={styles.tableCell}>{item.location}</td>
-                                        <td style={styles.tableCell}>{item.status}</td>
-                                        <td style={styles.tableCell}>{item.safeguard_date ? new Date(item.safeguard_date).toLocaleDateString() : 'N/A'}</td>
-                                        <td style={styles.tableCell}>{item.reason_not_assigned || 'N/A'}</td>
-                                        <td style={styles.tableCell}>{item.comment || 'N/A'}</td>
-                                        <td style={styles.tableCell}>{item.serial_number}</td>
-                                        <td style={styles.tableCell}>{item.inventory_number}</td>
+                                        <td className="tableCell">{item.name}</td>
+                                        <td className="tableCell">{item.description}</td>
+                                        <td className="tableCell">{item.category}</td>
+                                        <td className="tableCell">{new Date(item.entry_date).toLocaleDateString()}</td>
+                                        <td className="tableCell">{new Date(item.exit_date).toLocaleDateString()}</td>
+                                        <td className="tableCell">{item.location}</td>
+                                        <td className="tableCell">{item.status}</td>
+                                        <td className="tableCell">{item.safeguard_date ? new Date(item.safeguard_date).toLocaleDateString() : 'N/A'}</td>
+                                        <td className="tableCell">{item.reason_not_assigned || 'N/A'}</td>
+                                        <td className="tableCell">{item.comment || 'N/A'}</td>
+                                        <td className="tableCell">{item.serial_number}</td>
+                                        <td className="tableCell">{item.inventory_number}</td>
                                     </tr>
                                 ))}
                             </tbody>
-
-
                         </table>
                     </div>
                 </div>
@@ -140,97 +138,5 @@ function UserInventory() {
         </div>
     );
 }
-
-// Estilos en línea para los componentes
-const styles = {
-    card: {
-        backgroundColor: '#fff',
-        borderRadius: '8px',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        padding: '20px',
-        width: '300px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '10px',
-    },
-    userInfo: {
-        display: 'flex',
-        alignItems: 'center',
-        gap: '10px',
-        marginBottom: '10px',
-    },
-    userImage: {
-        borderRadius: '50%',
-        width: '50px',
-        height: '50px',
-    },
-    button: {
-        backgroundColor: '#007bff',
-        color: '#fff',
-        border: 'none',
-        borderRadius: '4px',
-        padding: '10px',
-        cursor: 'pointer',
-        textAlign: 'center',
-    },
-    userText: {
-        color: 'black',
-    },
-    modal: {
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    modalContent: {
-        backgroundColor: '#fff',
-        borderRadius: '8px',
-        padding: '20px',
-        width: '90%',
-        maxWidth: '1200px',
-        position: 'relative',
-        overflowX: 'auto',
-    },
-    closeButton: {
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-        cursor: 'pointer',
-        fontSize: '20px',
-    },
-    searchInput: {
-        width: '100%',
-        padding: '10px',
-        marginBottom: '10px',
-        border: '1px solid #ddd',
-        borderRadius: '4px',
-        boxSizing: 'border-box',
-    },
-    table: {
-        width: '100%',
-        borderCollapse: 'collapse',
-    },
-    tableHeaderCell: {
-        backgroundColor: '#007bff',
-        color: '#fff',
-        border: '1px solid #000',
-        padding: '12px',
-        textAlign: 'left',
-        fontWeight: 'bold',
-    },
-    tableRow: {
-        backgroundColor: '#ddd', // Set uniform background color for rows
-    },
-    tableCell: {
-        border: '1px solid #ddd',
-        padding: '8px',
-        textAlign: 'left',
-    },
-};
 
 export default UserInventory;
