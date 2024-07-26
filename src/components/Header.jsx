61ce1c7f911b7e0071d87310a6167ff74715a4c3
MendezPro <orlanmen58@gmail.com>
2024-07-26 09:35:29 -0600

    Cambios header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -240,7 +240,7 @@ const Header = () => {
             <p>{userInfo.tbc_charge?.name}</p>
           </div>
           <div>
-            <span>NUMERO DE TRABAJADOR</span>
+            <span>NUMERO</span>
             <p>{userInfo.worker_nomber}</p>
           </div>
         </div>
