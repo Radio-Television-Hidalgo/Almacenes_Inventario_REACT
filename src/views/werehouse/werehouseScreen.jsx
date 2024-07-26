9df45ef53e0ead50485dd4aee78ec4085d770c96
Martin Vargas Gutierrez <831martingutierrez@gmai.com>
2024-07-26 11:44:07 -0600

    Correcion de cambios

--- src/views/werehouse/werehouseScreen.jsx
+++ src/views/werehouse/werehouseScreen.jsx
@@ -27,7 +27,7 @@ export default function WerehouseScreen() {
               <h1>Entrada de existencias</h1>
             </div>
           </Link>
-          <Link to="" className="werehouse-card">
+          <Link to="/solicitudInsumos" className="werehouse-card">
             <div className="werehouse-content">
               <h1>Solicitudes de insumos</h1>
               <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-inboxes" viewBox="0 0 16 16">
@@ -79,18 +79,8 @@ export default function WerehouseScreen() {
             </svg>
             </div>
           </Link>
-          <Link to="/solicitudInsumos" className="werehouse-cards">
-            <div className="werehouse-content">
-              <h1>Solicitudes de insumos</h1>
-              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-inboxes" viewBox="0 0 16 16">
-              <path d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0z"/>
-            </svg>
-            </div>
-          </Link>
-
         </div>
       </div>
     </div>
   );
 }
-
