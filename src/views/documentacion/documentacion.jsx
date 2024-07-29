51ad42592902a6ffe719ec0e685adbad6b9434ce
Martin Vargas Gutierrez <831martingutierrez@gmai.com>
2024-07-29 11:30:58 -0600

    Cambio de diseño del menu del documentacion

--- src/views/documentacion/documentacion.jsx
+++ src/views/documentacion/documentacion.jsx
@@ -4,28 +4,50 @@ import "../../styles/documentacion.css";
 
 const Documentation = () => {
   return (
-    <div className="container">
+    <div className="documentacion-container">
       <br />
       <main className="main-content">
-        <h1>Documentación</h1>
+        <h1 className="h1-documentatio">Documentación</h1>
         
-        <div className="buttons-container">
-          <Link to="/facturas" className="card button">
-            <span className="icon">🚪</span>
-            <span>Ver facturas</span>
-          </Link>
-          <Link to="/crearFactura" className="card button">
-            <span className="icon">🚪</span>
-            <span>Crear Facturas</span>
-          </Link>
-          <Link to="/crearPoliza" className="card button">
-            <span className="icon">📄</span>
-            <span>Crear Poliza</span>
-          </Link>
-          <Link to="/polizas" className="card button">
-            <span className="icon">📂</span>
-            <span>Ver polizas</span>
-          </Link>
+        <div className="documentacion-columns">
+          <div className="documentacion-column documentacion-column-left">
+            <Link to="/facturas" className="documentacion-card button card-left">
+              <span className="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-clipboard2-fill" viewBox="0 0 16 16">
+                  <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z"/>
+                  <path d="M3.5 1h.585A1.5 1.5 0 0 0 4 1.5V2a1.5 1.5 0 0 0 1.5 1.5h5A1.5 1.5 0 0 0 12 2v-.5q-.001-.264-.085-.5h.585A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1"/>
+                </svg>
+                <h1 className="h1-mini">Ver facturas</h1>
+              </span>
+            </Link>
+            <Link to="/crearFactura" className="documentacion-card button card-left">
+              <span className="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-clipboard-plus-fill" viewBox="0 0 16 16">
+                  <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
+                  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5zm4.5 6V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5a.5.5 0 0 1 1 0"/>
+                </svg>
+                <h1 className="h1-mini">Crear Facturas</h1>
+              </span>
+            </Link>
+          </div>
+          <div className="documentacion-column documentacion-column-right">
+            <Link to="/crearPoliza" className="documentacion-card button card-right">
+              <span className="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
+                  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0"/>
+                </svg>
+                <h1 className="h1-mini">Crear Poliza</h1>
+              </span>
+            </Link>
+            <Link to="/polizas" className="documentacion-card button card-right">
+              <span className="icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-file-earmark-ruled-fill" viewBox="0 0 16 16">
+                  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M3 9h10v1H6v2h7v1H6v2H5v-2H3v-1h2v-2H3z"/>
+                </svg>
+                <h1 className="h1-mini">Ver polizas</h1>
+              </span>
+            </Link>
+          </div>
         </div>
       </main>
     </div>
