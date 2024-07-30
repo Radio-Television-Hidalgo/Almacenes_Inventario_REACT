1273bb82d5eafdf0b68f44424346377e2ea63550
Raul Reyes <137012254+RaulReyesB@users.noreply.github.com>
2024-07-30 16:21:57 -0600

    visualizacion de un campo

--- src/views/articles/createArticle.jsx
+++ src/views/articles/createArticle.jsx
@@ -40,53 +40,51 @@ function CreateArticle() {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-// CreateArticle.js
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const data = new FormData();
-    Object.keys(formData).forEach((key) => data.append(key, formData[key]));
-    files.forEach((file) => data.append("photos_entry", file));
 
-    const response = await fetch("/api/articulos/insertarArticulo", {
-      method: "POST",
-      body: data,
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = new FormData();
+      Object.keys(formData).forEach((key) => data.append(key, formData[key]));
+      files.forEach((file) => data.append("photos_entry", file));
 
-    if (!response.ok) {
-      throw new Error("Error creating article");
+      const response = await fetch("/api/articulos/insertarArticulo", {
+        method: "POST",
+        body: data,
+      });
+
+      if (!response.ok) {
+        throw new Error("Error creating article");
+      }
+
+      const result = await response.json();
+      console.log("Article created successfully:", result);
+      setFormData({
+        name: "",
+        brand: "",
+        model: "",
+        acquisition_date: "",
+        number_series: "",
+        status: "",
+        description: "",
+        caracteristics: "",
+        type: "",
+        userful_live_id: "",
+        policy_id: "",
+        bill_id: "",
+      });
+      setFiles([]);
+      setQrValue(result.name);
+
+      // Guardar el artículo en sessionStorage
+      sessionStorage.setItem("article", JSON.stringify(result));
+
+      // Redirigir a /articulos/almacen
+      window.location.href = `/articulos/almacen`;
+    } catch (error) {
+      console.error("Error:", error);
     }
-
-    const result = await response.json();
-    console.log("Article created successfully:", result);
-    setFormData({
-      name: "",
-      brand: "",
-      model: "",
-      acquisition_date: "",
-      number_series: "",
-      status: "",
-      description: "",
-      caracteristics: "",
-      type: "",
-      userful_live_id: "",
-      policy_id: "",
-      bill_id: "",
-    });
-    setFiles([]);
-    setQrValue(result.name);
-
-    // Guardar el artículo en sessionStorage
-    sessionStorage.setItem('article', JSON.stringify(result));
-
-    // Redirigir a /articulos/almacen
-    window.location.href = `/articulos/almacen`;
-
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
-
+  };
 
   useEffect(() => {
     const fetchCatalogs = async () => {
@@ -98,11 +96,11 @@ const handleSubmit = async (e) => {
         setUsefulLives(data.usefulLives);
         setPolicies(data.policies);
         setBills(data.bills);
-        console.log(data.policies)
+        console.log(data.policies);
         // Obtener todos los artículos
         const billsResponse = await fetch("/api/articulos/articulos");
         const articles = await billsResponse.json();
-  
+
         // Contar artículos por factura
         const articleCountByBill = articles.reduce((acc, article) => {
           acc[article.bill_id] = (acc[article.bill_id] || 0) + 1;
@@ -113,28 +111,27 @@ const handleSubmit = async (e) => {
           acc[article.policy_id] = (acc[article.policy_id] || 0) + 1;
           return acc;
         }, {});
-  
+
         // Filtrar las facturas disponibles
-        const availableBills = data.bills.filter(bill => {
+        const availableBills = data.bills.filter((bill) => {
           const articleCount = articleCountByBill[bill.id] || 0;
           return articleCount < bill.quantity;
         });
         //filtrar las polizas disponibles
-        const availablePolicies = data.policies.filter(policy => {
+        const availablePolicies = data.policies.filter((policy) => {
           const articleCount = articleCountByPolicy[policy.id] || 0;
-          return articleCount < policy.quantity;
+          return accounapoliciesCount < policy.quantity;
         });
-  
+
         setAvailableBills(availableBills);
         setAvailablePolicies(availablePolicies);
       } catch (error) {
         console.error("Error fetching catalogs:", error);
       }
     };
-  
+
     fetchCatalogs();
   }, []);
-  
 
   return (
     <div className="main-container2">
@@ -235,22 +232,24 @@ const handleSubmit = async (e) => {
               <option value="Bien">Bien</option>
             </select>
           </div>
-          <div className="form-group2">
-            <label>Vida útil:</label>
-            <select
-              name="userful_live_id"
-              value={formData.userful_live_id}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Selecciona vida útil</option>
-              {usefulLives.map((life) => (
-                <option key={life.id} value={life.id}>
-                  {life.concept}
-                </option>
-              ))}
-            </select>
-          </div>
+          {formData.type !== "Insumos" && (
+            <div className="form-group2">
+              <label>Vida útil:</label>
+              <select
+                name="userful_live_id"
+                value={formData.userful_live_id}
+                onChange={handleChange}
+                required
+              >
+                <option value="">Selecciona vida útil</option>
+                {usefulLives.map((life) => (
+                  <option key={life.id} value={life.id}>
+                    {life.concept}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
           <div className="form-group2">
             <label>Póliza:</label>
             <select
@@ -283,38 +282,44 @@ const handleSubmit = async (e) => {
               ))}
             </select>
           </div>
-        <div className="form-group2" {...getRootProps({ className: "dropzone" })}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <p>Suelta los archivos aquí...</p>
-          ) : (
-            <p>Arrastre y suelte algunos archivos aquí o haga clic para seleccionar archivos</p>
+          <div
+            className="form-group2"
+            {...getRootProps({ className: "dropzone" })}
+          >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Suelta los archivos aquí...</p>
+            ) : (
+              <p>
+                Arrastre y suelte algunos archivos aquí o haga clic para
+                seleccionar archivos
+              </p>
+            )}
+            <div className="files-preview">
+              {files.map((file, index) => (
+                <div key={index} className="file-item">
+                  {file.type.startsWith("image/") ? (
+                    <img src={URL.createObjectURL(file)} alt={file.name} />
+                  ) : (
+                    <span>{file.name}</span>
+                  )}
+                  <button type="button" onClick={() => removeFile(file)}>
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          {qrValue && (
+            <div className="qr-container">
+              <QRCode value={qrValue} />
+            </div>
           )}
-          <div className="files-preview">
-            {files.map((file, index) => (
-              <div key={index} className="file-item">
-                {file.type.startsWith("image/") ? (
-                  <img src={URL.createObjectURL(file)} alt={file.name} />
-                ) : (
-                  <span>{file.name}</span>
-                )}
-                <button type="button" onClick={() => removeFile(file)}>
-                  Remove
-                </button>
-              </div>
-            ))}
-          </div>
         </div>
-        {qrValue && (
-          <div className="qr-container">
-            <QRCode value={qrValue} />
-          </div>
-        )}
-      </div>
-    <button type="submit" className="submit-button">
-      Crear articulo
-    </button>
-  </form>
+        <button type="submit" className="submit-button">
+          Crear articulo
+        </button>
+      </form>
     </div>
   );
 }
