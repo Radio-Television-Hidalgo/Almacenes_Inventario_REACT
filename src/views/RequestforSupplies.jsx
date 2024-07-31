170f6dacf243bac5272a077a7efa8bb2f22cf5d8
ZaS-ex <a.zacek.gc@gmail.com>
2024-07-31 16:48:09 -0600

    añadido de modal de solicitud

--- src/views/RequestforSupplies.jsx
+++ src/views/RequestforSupplies.jsx
@@ -5,6 +5,7 @@ import "../styles/RequestforSupplies.css";
 
 function RequestforSupplies() {
   const [datos, setDatos] = useState([]);
+  const [modalData, setModalData] = useState(null);
 
   const fetchDatos = () => {
     fetch("/api/solicitud/solicitudesInsumos")
@@ -65,6 +66,14 @@ function RequestforSupplies() {
     } 
   };
 
+  const handleViewMore = (dato) => {
+    setModalData(dato);
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+  };
+
   return (
     <div className="request-for-supplies-container">
       <table className="request-for-supplies-table">
@@ -87,7 +96,7 @@ function RequestforSupplies() {
               <td className="request-for-supplies-cell"> {dato.quantity} </td>
               <td className="request-for-supplies-cell"> {dato.description} </td>
               <td className="request-for-supplies-cell"> {dato.status} </td>
-              <td className="request-for-supplies-cell"> <button className="request-for-supplies-button request-for-supplies-view-button">Ver solicitud</button> </td>
+              <td className="request-for-supplies-cell"> <button onClick={() => handleViewMore(dato)} className="request-for-supplies-button request-for-supplies-view-button">Ver solicitud</button> </td>
               <td className="request-for-supplies-cell">
                 <div className="request-for-supplies-button-container">
                   <button onClick={(event) => handleAcept(event, dato.id)} className="request-for-supplies-button request-for-supplies-accept-button">Aceptar Solicitud</button>
@@ -98,6 +107,32 @@ function RequestforSupplies() {
           ))}
         </tbody>
       </table>
+      {modalData && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close-button" onClick={closeModal}>&times;</span>
+            <h2> <strong>Detalle de la Solicitud</strong> </h2>
+            <div>
+              <h2>Artículo: </h2>
+              <img src={`${modalData.requestArticle.photos_entry}`}  alt="" />
+              <p><strong>{modalData.requestArticle.name}</strong></p>
+              <p><strong>Marca: </strong> {modalData.requestArticle.brand}</p>
+              <p><strong>Modelo: </strong> {modalData.requestArticle.model}</p>
+              <p><strong>Numero de serie: </strong> {modalData.requestArticle.number_series}</p>
+              <p><strong>Descripción: </strong> {modalData.requestArticle.description}</p>
+              <p><strong>QR: </strong> {modalData.requestArticle.QR}</p>
+              <p><strong></strong> </p>
+            </div>
+            <div>
+              <h2>Solicita: </h2>
+              <img src={`${modalData.requestingUser.img}`}  alt="" />
+              <p><strong>{modalData.requestingUser.name}</strong></p>
+              <p><strong>Numero de trabajador:</strong>{modalData.requestingUser.worker_nomber}</p>
+            </div>
+            
+          </div>
+        </div>
+      )}  
     </div>
   );
 }
